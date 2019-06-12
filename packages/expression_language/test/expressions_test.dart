a8d72c1ed786f272026f106b509b58444a036ff0
ondrejkunc <ondrej.kunc@embedit.cz>
2019-06-12 12:25:30 +0200

    Add code coverage

---
+++ packages/expression_language/test/expressions_test.dart
@@ -0,0 +1,32 @@
+import 'dart:async';
+import 'package:gherkin/gherkin.dart';
+import 'package:glob/glob.dart';
+import 'supporting_files/hooks/hook_example.dart';
+import 'supporting_files/steps/when_then_expression_num.dart';
+
+void main() async {
+      final config = TestConfiguration()
+      ..features = [Glob(r"test/features/**.feature")]
+      ..exitAfterTestRun = false
+      ..reporters = [
+        StdoutReporter(MessageLevel.error),
+        ProgressReporter(),
+        TestRunSummaryReporter(),
+        JsonReporter(path: 'test/report.json')
+      ]
+      ..hooks = [HookExample()]
+      ..createWorld = (TestConfiguration config) {
+        return Future.value(ExpressionWorld());
+      }
+      ..stepDefinitions = [
+        GivenFormElementIsProvided(),
+        WhenExpressionIsEvaluated(),
+        ThenIntExpressionResult(),
+        ThenDecimalExpressionResult(),
+        ThenBoolExpressionResult(),
+        ThenExceptionThrownResult()
+      ];
+
+    var runner = GherkinRunner();
+    await runner.execute(config);
+}
