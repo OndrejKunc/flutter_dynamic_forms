caaa666ba993c10b739d8590af468a524b73d61c
ondrejkunc <kunc.ondrej1@gmail.com>
2019-09-10 13:18:27 +0200

    Add basic generator structure

---
+++ packages/dynamic_forms_generator/lib/src/parser_generator.dart
@@ -0,0 +1,11 @@
+import 'package:dynamic_forms_generator/src/component_description.dart';
+
+class ParserGenerator {
+  final ComponentDescription componentDescription;
+
+  ParserGenerator(this.componentDescription);
+
+  String generate() {
+    //TODO
+  }
+}
