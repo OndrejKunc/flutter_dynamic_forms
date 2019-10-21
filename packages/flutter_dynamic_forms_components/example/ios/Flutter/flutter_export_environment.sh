2dfd2c656725cf414be389a1e65ba7cc74853794
unknown <yasinilhan61@gmail.com>
2019-10-21 14:28:51 +0300

    change dateValue to value property name added initialData value

--- packages/flutter_dynamic_forms_components/example/ios/Flutter/flutter_export_environment.sh
+++ packages/flutter_dynamic_forms_components/example/ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/yasinilhan/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/yasinilhan/Desktop/Projeler/flprojeler/flutter_dynamic_forms/packages/flutter_dynamic_forms_components/example"
-export "FLUTTER_TARGET=/Users/yasinilhan/Desktop/Projeler/flprojeler/flutter_dynamic_forms/packages/flutter_dynamic_forms_components/example/lib/main.dart"
+export "FLUTTER_ROOT=C:\Projeler\mobile\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Projeler\mobile\flutterapp\flutter_dynamic_forms\packages\flutter_dynamic_forms_components\example"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/yasinilhan/flutter/bin/cache/artifacts/engine/ios"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=C:\Projeler\mobile\flutter\bin\cache\artifacts\engine\ios"
 export "FLUTTER_BUILD_NAME=0.2.0"
 export "FLUTTER_BUILD_NUMBER=0.2.0"
