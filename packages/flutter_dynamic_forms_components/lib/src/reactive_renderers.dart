f80ca21a034c7ef11b06a5b5a83f5b605c3169df
Yasin ilhan <yasinilhan61@gmail.com>
2019-11-01 10:50:34 +0300

    fix conflicts

--- packages/flutter_dynamic_forms_components/lib/src/reactive_renderers.dart
+++ packages/flutter_dynamic_forms_components/lib/src/reactive_renderers.dart
@@ -17,6 +17,7 @@ List<FormElementRenderer> getReactiveRenderers() {
     ReactiveMultiSelectChipGroupRenderer(),
     ReactiveDateRenderer(),
     ReactiveDateRangeRenderer(),
+    ReactiveSliderRenderer(),
 
   ];
 }
