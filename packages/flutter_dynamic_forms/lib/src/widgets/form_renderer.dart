8ec7859c3067b6ff061372c28781b2d96878420e
ondrejkunc <kunc.ondrej1@gmail.com>
2020-08-03 13:30:30 +0200

    Add explicit FormManager parameter to FormRender

--- packages/flutter_dynamic_forms/lib/src/widgets/form_renderer.dart
+++ packages/flutter_dynamic_forms/lib/src/widgets/form_renderer.dart
@@ -1,14 +1,17 @@
+import 'package:dynamic_forms/dynamic_forms.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_dynamic_forms/flutter_dynamic_forms.dart';
 
 class FormRenderer extends StatefulWidget {
   final List<FormElementRenderer> renderers;
   final FormElementEventDispatcherFunction dispatcher;
+  final FormManager formManager;
 
   const FormRenderer({
     Key key,
     @required this.renderers,
     this.dispatcher,
+    this.formManager,
   }) : super(key: key);
 
   @override
@@ -17,11 +20,23 @@ class FormRenderer extends StatefulWidget {
 
 class _FormRendererState extends State<FormRenderer> {
   FormRenderService _formRenderService;
-  FormData _dynamicFormsData;
+  FormManager _formManager;
 
   @override
-  void initState() {
-    super.initState();
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    if (widget.formManager == null) {
+      _formManager = FormProvider.of(context)?.formManager;
+      if (_formManager == null) {
+        throw Exception('''
+Error: No `FormManager` provided to the `FormRenderer` widget. 
+Please pass `FormManager` as the parameter to the `FormRenderer` 
+or make sure you have `FormProvider` as your ancestor widget.
+  ''');
+      }
+    } else {
+      _formManager = widget.formManager;
+    }
     _formRenderService = FormRenderService(
       renderers: widget.renderers,
       dispatcher: widget.dispatcher ?? _onFormElementEvent,
@@ -29,20 +44,13 @@ class _FormRendererState extends State<FormRenderer> {
   }
 
   @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    _dynamicFormsData = FormProvider.of(context);
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return _formRenderService.render(
-        _dynamicFormsData.formManager.form, context);
+    return _formRenderService.render(_formManager.form, context);
   }
 
   void _onFormElementEvent(FormElementEvent event) {
     if (event is ChangeValueEvent) {
-      _dynamicFormsData.formManager.changeValue(
+      _formManager.changeValue(
           value: event.value,
           elementId: event.elementId,
           propertyName: event.propertyName,
