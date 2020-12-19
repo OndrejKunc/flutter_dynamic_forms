5536319d7002c1a984d6d46dcd99b5239d693458
Ondrej Kunc <kunc.ondrej1@gmail.com>
2020-12-19 01:14:23 +0100

    Fix tests

---
+++ packages/expression_language/test/expressions_test.dart
@@ -0,0 +1,32 @@
+import 'dart:async';
+import 'package:gherkin/gherkin.dart';
+import 'package:glob/glob.dart';
+import 'supporting_files/steps/when_then_expression_num.dart';
+
+void main() async {
+  final config = TestConfiguration()
+    ..features = [Glob(r'test/features/**.feature')]
+    ..exitAfterTestRun = false
+    ..reporters = [
+      StdoutReporter(MessageLevel.error),
+      ProgressReporter(),
+      TestRunSummaryReporter(),
+      JsonReporter(path: 'test/report.json')
+    ]
+    ..createWorld = (TestConfiguration config) {
+      return Future.value(ExpressionWorld());
+    }
+    ..stepDefinitions = [
+      GivenFormElementIsProvided(),
+      WhenExpressionIsEvaluated(),
+      ThenIntExpressionResult(),
+      ThenStringExpressionResult(),
+      ThenDecimalExpressionResult(),
+      ThenBoolExpressionResult(),
+      ThenExceptionThrownResult(),
+      ThenDateTimeExpressionResult(),
+    ];
+
+  var runner = GherkinRunner();
+  await runner.execute(config);
+}
