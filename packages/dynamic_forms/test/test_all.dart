4501c7dcb3428dcf2fd595dbcbf96471ff38e32a
Ondrej Kunc <kunc.ondrej1@gmail.com>
2021-04-05 17:14:35 +0200

    Fix test coverage

---
+++ packages/dynamic_forms/test/test_all.dart
@@ -0,0 +1,13 @@
+// @dart=2.9
+
+import 'expression_parser_test.dart' as expression_parser_test;
+import 'form_builder_test.dart' as form_builder_test;
+import 'json_parser_test.dart' as json_parser_test;
+import 'xml_parser_test.dart' as xml_parser_test;
+
+void main() {
+  expression_parser_test.main();
+  form_builder_test.main();
+  json_parser_test.main();
+  xml_parser_test.main();
+}
